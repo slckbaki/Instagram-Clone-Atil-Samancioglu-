a250483699b7f40eefd9aa70bd487667f0e94846
selcuk baki <selcukbaki@windowslive.com>
2021-08-23 23:49:57 +0300

    Initial Commit

---
+++ 19-Instagram Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  19-Instagram Clone
+//
+//  Created by Selcuk Baki on 23/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
