6d312a976259552a9da38ff65b63844f92be56b1
selcuk baki <selcukbaki@windowslive.com>
2021-08-28 17:12:43 +0300

    first commit

---
+++ 19-Instagram Clone/View Controllers/FeedCell.swift
@@ -0,0 +1,23 @@
+//
+//  FeedCell.swift
+//  19-Instagram Clone
+//
+//  Created by Selcuk Baki on 27/8/21.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
